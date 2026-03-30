f6801930c865feedd13c82dd4e5c28e9e20c1017
Dmitriy Drozdov <149553535+TovarishLenin@users.noreply.github.com>
2026-03-30 18:25:05 +0500

    Update copy_db.py

--- copy_db.py
+++ copy_db.py
@@ -1,19 +1,14 @@
 import sqlite3
 
-# Подключаемся к исходной базе данных
 conn_source = sqlite3.connect('skin_bot.db')
 
-    # Создаём подключение к новой базе данных
 conn_target = sqlite3.connect('copy.db')
 
 try:
-    # Подключаемся к исходной базе данных
     conn_source = sqlite3.connect('skin_bot.db')
 
-    # Создаём подключение к новой базе данных
     conn_target = sqlite3.connect('copy.db')
 
-    # Выполняем операцию VACUUM INTO
     conn_source.backup(conn_target)
 
     print("Копирование завершено успешно!")
